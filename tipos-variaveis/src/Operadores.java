fa45f797bfabe273044c63d98d06f03f66b2d9c3
alex <alexvieira.360@hotmail.com>
2024-05-03 14:55:06 -0300

    commit trabalhando com operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,34 @@
+public class Operadores {
+    public static void main(String[] args) {
+        /*String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println(nomeCompleto); 
+
+        int numero = 5;
+
+        numero = - numero;
+        System.out.println(-numero);
+
+        System.out.println(numero);
+
+        numero = + numero * -1;
+
+        System.out.println(numero);
+
+        int numero = 5;
+
+        // repetição
+        
+        System.out.println(numero--);
+
+        System.out.println(numero);
+
+        boolean variavel = true;
+
+        variavel = !variavel;
+
+        System.out.println(variavel);*/
+
+
+
+    }
+}
