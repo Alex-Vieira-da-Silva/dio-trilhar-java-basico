a0b4cd476f837f778a5543b15c9a0acc71b95c8a
alex <alexvieira.360@hotmail.com>
2024-05-07 18:24:06 -0300

    commit operadores finalizado

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,34 +1,74 @@
 public class Operadores {
     public static void main(String[] args) {
-        /*String nomeCompleto = "LINGUAGEM" + "JAVA";
-        System.out.println(nomeCompleto); 
+        /*
+         * String nomeCompleto = "LINGUAGEM" + "JAVA";
+         * System.out.println(nomeCompleto);
+         * 
+         * int numero = 5;
+         * 
+         * numero = - numero;
+         * System.out.println(-numero);
+         * 
+         * System.out.println(numero);
+         * 
+         * numero = + numero * -1;
+         * 
+         * System.out.println(numero);
+         * 
+         * int numero = 5;
+         * 
+         * // repetição
+         * 
+         * System.out.println(numero--);
+         * 
+         * System.out.println(numero);
+         * 
+         * boolean variavel = true;
+         * 
+         * variavel = !variavel;
+         * 
+         * System.out.println(variavel);
+         * 
+         * 
+         * int a, b;
+         * 
+         * a = 5;
+         * b = 5;
+         * 
+         * int resultado = a==b ? 1 : 0;
+         * 
+         * System.out.println(resultado);
+         * 
+         * 
+         * String nomeUm = "Alex";
+         * String nomeDois = new String("Alex");
+         * 
+         * System.out.println(nomeDois.equals(nomeUm));
+         * 
+         * int numero1 = 1;
+         * int numero2 = 2;
+         * 
+         * boolean simNao = numero1 != numero2;
+         * 
+         * if (numero1 < numero2)
+         * System.out.println("A nossa codição é verdadeira");
+         * else if (numero1 == numero2)
+         * System.out.println("A nossa codição é falsa");
+         * 
+         * System.out.println("numeroUm é diferente a numeroDois? " + simNao);
+         * 
+         * simNao = numero1 > numero2;
+         * 
+         * System.out.println("numeroUm é maior que numeroDois? " + simNao);
+         */
 
-        int numero = 5;
+        boolean condicao1 = true;
+        boolean condicao2 = false;
 
-        numero = - numero;
-        System.out.println(-numero);
-
-        System.out.println(numero);
-
-        numero = + numero * -1;
-
-        System.out.println(numero);
-
-        int numero = 5;
-
-        // repetição
-        
-        System.out.println(numero--);
-
-        System.out.println(numero);
-
-        boolean variavel = true;
-
-        variavel = !variavel;
-
-        System.out.println(variavel);*/
-
-
+        if (condicao1 && (7 > 4))
+            System.out.println("As duas condições são verdadeiras.");
+        if (condicao1 || condicao2)
+            System.out.println("Uma das condições são verdadeiras.");
 
     }
 }
